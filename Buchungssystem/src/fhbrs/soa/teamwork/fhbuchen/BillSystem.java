c52da9cc44aa3e7870ef098c643a2bd7e7531f1c
HAPM <alpha@ircshark.net>
2011-05-16 12:34:40 +0200

    added annotations to BillSystem and InvoiceSystem

--- Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/BillSystem.java
+++ Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/BillSystem.java
@@ -3,20 +3,29 @@ package fhbrs.soa.teamwork.fhbuchen;
 import java.util.Date;
 import java.util.Set;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class BillSystem {
+	@WebMethod
 	Bill searchBill(int billNr) {
 		return null;
 	}
 
+	@WebMethod
 	Set<Bill> listBills() {
 		return null;
 	}
 
+	@WebMethod
 	Bill createBill(int billNr, Date date, String author, String reciever,
 			Set<BillPosition> positions) {
 		Bill b = new Bill(billNr, date, author, reciever, positions);
 		return b;
 	}
+	
+	@WebMethod
 	void updateBill(Bill b){
 		
 	}
--- Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/InvoiceSystem.java
+++ Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/InvoiceSystem.java
@@ -3,16 +3,27 @@ package fhbrs.soa.teamwork.fhbuchen;
 import java.util.Date;
 import java.util.Set;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class InvoiceSystem {
+	@WebMethod
 	Invoice searchInvoice(int invoiceNr){
 		return null;
 	}
+	
+	@WebMethod
 	Set<Invoice> listInvoices(){
 		return null;
 	}
+	
+	@WebMethod
 	void updateInvoice(Invoice i){
 		
 	}
+	
+	@WebMethod
 	Invoice createInvoice(int billNr, Date date, String author, String reciever,
 			Set<InvoicePosition> positions){
 		Invoice i = new Invoice(billNr, date,author,reciever,positions);
