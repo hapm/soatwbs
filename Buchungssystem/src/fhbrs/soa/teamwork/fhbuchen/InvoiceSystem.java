40b076f56c9e8c15332ff437ee62b3dd302fc2ac
David Goering <dgoering@gmail.com>
2011-05-16 13:15:00 +0200

    Added Access modifiers to webmethods

--- Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/InvoiceSystem.java
+++ Buchungssystem/src/fhbrs/soa/teamwork/fhbuchen/InvoiceSystem.java
@@ -9,22 +9,22 @@ import javax.jws.WebService;
 @WebService
 public class InvoiceSystem {
 	@WebMethod
-	Invoice searchInvoice(int invoiceNr){
+	public Invoice searchInvoice(int invoiceNr){
 		return null;
 	}
 	
 	@WebMethod
-	Set<Invoice> listInvoices(){
+	public Set<Invoice> listInvoices(){
 		return null;
 	}
 	
 	@WebMethod
-	void updateInvoice(Invoice i){
+	public void updateInvoice(Invoice i){
 		
 	}
 	
 	@WebMethod
-	Invoice createInvoice(int billNr, Date date, String author, String reciever,
+	public Invoice createInvoice(int billNr, Date date, String author, String reciever,
 			Set<InvoicePosition> positions){
 		Invoice i = new Invoice(billNr, date,author,reciever,positions);
 		return i;
